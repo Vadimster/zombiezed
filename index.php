01ec429af7bfafdd42f51133f8155d4ff7f1486c
Vadimster <vadimke@gmail.com>
2016-06-04 12:34:30 +0200

    upload of file structure

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+	#include ('db.php');
+	#$dataBase = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);	
+	#PREPARE AND TRY	
+	#$statement = $dataBase->query('SELECT * FROM users');
+	#$rowCount = $statement->rowCount();
+	#echo 'There are currently ' . $rowCount . ' rows in the database';	
+?>
+<HTML>
+	<HEAD>
+		<meta charset="utf-8">
+		<TITLE>title</TITLE>
+		<script type="text/javascript" src="jquery/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="jquery/jquery-ui.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="styles/main.css" />
+		<link rel="stylesheet" href="jquery/jquery-ui.min.css" /> 
+		<link rel="stylesheet" href="jquery/jquery-ui-config.css" /> 		
+	</HEAD>
+	<BODY>
+
+
+
+
+
+
+		<script type="text/javascript" src="js/main.js"></script>
+	</BODY>
+</HTML>
+
---
+++ php/db.php
@@ -0,0 +1,10 @@
+<?php
+
+//Database credentials
+
+	$dbHost = 'localhost';
+	$dbName = 'vadimlv_greenelephant';
+	$dbUser = 'vadimlv_root';
+	$dbPass = 'K86gf%tgW8';
+
+?>
