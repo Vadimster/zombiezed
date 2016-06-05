f307760239d970cfae64eb575ceaecca3678c50b
Vadimster <vadimke@gmail.com>
2016-06-05 11:40:57 +0200

    removed changes in index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	</HEAD>
 	<BODY>
 
-<p>This is a test environment for ZombieZed. New change</p>
+<p>This is a test environment for ZombieZed.</p>
 
 
 		<script type="text/javascript" src="js/main.js"></script>
