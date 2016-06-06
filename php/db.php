379be3ef43471c5d19c4344b293e735c906db2fa
Hacker <Hacker>
2016-06-06 18:48:28 +0200

    Hello added in index.php

--- php/db.php
+++ php/db.php
@@ -7,4 +7,25 @@
 	$dbUser = 'vadimlv_root';
 	$dbPass = 'K86gf%tgW8';
 
+
+
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
